d126aed6a8886218739b9d5d44183b01d330376d
Çağrı <karpuzcagri@gmail.com>
2024-02-05 17:55:48 +0300

    Main

--- JavaScript advance function 2/main.js
+++ JavaScript advance function 2/main.js
@@ -1,3 +1,4 @@
+/*
 const arrowFunction = (param, param2) => {
   console.log("Hello");
   return 5;
@@ -21,3 +22,23 @@ oneParam(2);
 const oneLine = () => 2 + 3;
 
 console.log(oneLine());
+
+const objent2 = {
+  method() {
+
+  }
+};
+*/
+
+const buttonElement = document.querySelector(".button-js");
+
+const eventListener = () => {
+  console.log("click");
+}
+buttonElement.addEventListener("click", eventListener);
+
+buttonElement.removeEventListener("click", eventListener);
+
+buttonElement.addEventListener("click", () => {
+  console.log("click2");
+});
